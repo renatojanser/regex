f09a352fdf7c5a3323ce1febb03a4f0fae36cd47
Renato Janser <renato.janser@gmail.com>
2020-01-12 14:59:09 -0300

    Finalizei a mascara de cnpj e telefone

--- mask/app.js
+++ mask/app.js
@@ -2,10 +2,27 @@ const masks = {
     cpf(value) {
         return value
             .replace(/\D/g, '')
-            .replace(/(\d{3})(\d{1})/, '$1.$2')
-            .replace(/(\d{3})(\d{1})/, '$1.$2')
-            .replace(/(\d{3})(\d{1})/, '$1-$2')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d)/, '$1-$2')
             .replace(/(-\d{2})(\d)/, '$1')
+    },
+    cnpj(value) {
+        return value
+            .replace(/\D/g, '')
+            .replace(/(\d{2})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d)/, '$1/$2')
+            .replace(/(\d{4})(\d)/, '$1-$2')
+            .replace(/(-\d{2})(\d)/, '$1')
+    },
+    telefone(value) {
+        return value
+            .replace(/\D/g, '')
+            .replace(/(\d{2})(\d)/, '($1) $2')
+            .replace(/(\d{4})(\d)/, '$1-$2')
+            .replace(/(\d{4})-(\d)(\d{4})+/, '$1$2-$3')
+            .replace(/(-\d{4})(\d)/, '$1')
     }
 }
 
