3669354a20244519e4052ddf0512564758cdefdf
Renato Janser <renato.janser@gmail.com>
2020-01-12 21:24:21 -0300

    Finalizei as mascar de cep, pis e data

--- mask/app.js
+++ mask/app.js
@@ -23,6 +23,27 @@ const masks = {
             .replace(/(\d{4})(\d)/, '$1-$2')
             .replace(/(\d{4})-(\d)(\d{4})+/, '$1$2-$3')
             .replace(/(-\d{4})(\d)/, '$1')
+    },
+    cep(value) {
+        return value
+            .replace(/\D/g, '')
+            .replace(/(\d{5})(\d)/, '$1-$2')
+            .replace(/(-\d{3})(\d)/, '$1')
+    },
+    pis(value) {
+        return value
+            .replace(/\D/g, '')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{5})(\d)/, '$1.$2')
+            .replace(/(\d{5})\.(\d{2})(\d)/, '$1.$2-$3')
+            .replace(/(-\d{1})(\d)/, '$1')
+    },
+    data(value) {
+        return value
+            .replace(/\D/g, '')
+            .replace(/(\d{2})(\d)/, '$1/$2')
+            .replace(/(\/\d{2})(\d)/, '$1/$2')
+            .replace(/(\/\d{4})(\d)/, '$1')
     }
 }
 
